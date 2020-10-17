79c82f74ac88daeedb0d5365fbe730c55b825ea5
fathyelshemy <fathyelshemy8@gmail.com>
2020-10-17 19:09:01 +0200

    fix ploymorphic example hands-on & try map with typeReference

--- src/test/java/com/jackson/bealdung/material/JacksonBealdungMaterialApplicationTests.java
+++ src/test/java/com/jackson/bealdung/material/JacksonBealdungMaterialApplicationTests.java
@@ -1,11 +1,15 @@
 package com.jackson.bealdung.material;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.ReferenceType;
 import com.jackson.bealdung.material.dto.JacksonPolymrophic.Animal;
 import com.jackson.bealdung.material.dto.JacksonPolymrophic.Cat;
 import com.jackson.bealdung.material.dto.JacksonPolymrophic.Dog;
 import com.jackson.bealdung.material.dto.deserialize.User;
+import com.jackson.bealdung.material.dto.objectMapperex.Car;
 import com.jackson.bealdung.material.dto.serialize.ExtendedBean;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,6 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
@@ -57,20 +62,61 @@ class JacksonBealdungMaterialApplicationTests {
 
     }
 
-    //TODO fix the error here
-
-//    @Test
+    @Test
     public void testJacksonPolymorphicByCat() throws JsonProcessingException {
-        String jsonCat="{\"animal\":{\"type\":\"cat\",\"name\":\"lacy\"}}";
+        String jsonCat="{\"type\":\"cat\",\"name\":\"lacy\"}";
         Cat cat= new ObjectMapper().readerFor(Animal.class).readValue(jsonCat);
         Assertions.assertEquals(cat.getName(),"lacy");
     }
-    //TODO fix the error here
-//    @Test
+    @Test
     public void testJacksonPolymorphicByDog() throws JsonProcessingException {
-        String jsonDog="{\"animal\":{\"type\":\"dog\",\"name\":\"lacy\",\"barkVolume\":0}}";
-        Animal dog= new ObjectMapper().readerFor(Animal.class).readValue(jsonDog);
+        String jsonDog="{\"type\":\"dog\",\"name\":\"lacy\",\"sound\":0}";
+        Dog dog= new ObjectMapper().readValue(jsonDog,Dog.class);
         Assertions.assertEquals(dog.getName(),"lacy");
+    }
+
+    @Test
+    public void testObjectMapperWithSerialize() throws JsonProcessingException {
+        Car car= new Car("red","BMW");
+        String jsonCar=new ObjectMapper().writeValueAsString(car);
+        Assertions.assertEquals(jsonCar,"{\"color\":\"red\",\"type\":\"BMW\"}");
+    }
+
+    @Test
+    public void testObjectMapperWithDeserialize() throws JsonProcessingException {
+        String jsonCar="{\"color\":\"black\",\"type\":\"Mercedes\"}";
+        Car car= new ObjectMapper().readerFor(Car.class).readValue(jsonCar);
+
+        Assertions.assertNotNull(car);
+        Assertions.assertEquals(car.getColor(),"black");
+        Assertions.assertEquals(car.getType(),"Mercedes");
+    }
+
+    @Test
+    public void testObjectMapperWithJsonNode() throws JsonProcessingException {
+        String jsonCar="{\"color\":\"black\",\"type\":\"Mercedes\"}";
+        JsonNode car=new ObjectMapper().readTree(jsonCar);
+
+        Assertions.assertNotNull(car);
+        Assertions.assertEquals(car.get("color").asText(),"black");
+        Assertions.assertEquals(car.get("type").asText(),"Mercedes");
+
+    }
+
+    @Test
+    public void testObjectMapperWithTypeReference() throws JsonProcessingException {
+        // List
+        String jsonCarArray = "[{ \"color\" : \"Black\", \"type\" : \"BMW\" }, { \"color\" : \"Red\", \"type\" : \"FIAT\" }]";
+
+        List<Car> cars=new ObjectMapper().readValue(jsonCarArray,new TypeReference<List<Car>>(){});
+        Assertions.assertEquals(cars.get(0).getType(),"BMW");
+        Assertions.assertEquals(cars.get(0).getColor(),"Black");
+        Assertions.assertEquals(cars.get(1).getType(), "FIAT");
+        Assertions.assertEquals(cars.get(1).getColor(),"Red");
+        // Map
+        String jsonCar="{\"color\":\"black\",\"type\":\"Mercedes\"}";
+        Map<String,String> carMap= new ObjectMapper().readValue(jsonCar, new TypeReference<Map<String, String>>() { });
+        Assertions.assertEquals(carMap.get("color"),"black");
     }
 
 }
